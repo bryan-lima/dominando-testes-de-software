6f89416f225638050d096e7ec8d1dcca764a5417
Bryan <bryanlima200@outlook.com>
2022-08-15 22:54:45 -0300

    Adicionado ClienteTests e aplicado Traits e Fact com DisplayName para categorizar e nomear melhor os testes

---
+++ 01 - Testes de Unidade/Features.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,56 @@
+﻿using Features.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Tests
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Bryan",
+                "Lima",
+                DateTime.Now.AddYears(-25),
+                "bryan@lima.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(condition: result);
+            Assert.Equal(expected: 0, actual: cliente.ValidationResult.Errors.Count);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInValido()
+        {
+            // Arrange
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now,
+                "bryan2lima.com",
+                true,
+                DateTime.Now);
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(condition: result);
+            Assert.NotEqual(expected: 0, actual: cliente.ValidationResult.Errors.Count);
+        }
+    }
+}
