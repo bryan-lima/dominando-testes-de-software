216c2cc37fb3bf9d3a286ea446d62bb9f8d27ed0
Bryan <bryanlima200@outlook.com>
2022-08-16 23:17:33 -0300

    Adicionado testes usando lib Bogus, para criação de dados fakes de maneira dinâmica

---
+++ 01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -0,0 +1,77 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteBogusCollection))]
+    public class ClienteBogusCollection : ICollectionFixture<ClienteTestsBogusFixture>
+    { }
+
+    public class ClienteTestsBogusFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            return GerarClientes(1, true).FirstOrDefault();
+        }
+
+        public IEnumerable<Cliente> ObterClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+
+            return clientes;
+        }
+
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var email = new Faker().Internet.Email(firstName: "bryan", lastName: "lima", provider: "gmail");
+            var clientefaker = new Faker<Cliente>();
+            clientefaker.RuleFor(property: c => c.Nome, setter: (f, c) => f.Name.FirstName());
+
+            var clientes = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(yearsToGoBack: 80, refDate: DateTime.Now.AddYears(-18)),
+                    "",
+                    ativo,
+                    DateTime.Now))
+                .RuleFor(property: c => c.Email, setter: (f, c) =>
+                    f.Internet.Email(firstName: c.Nome.ToLower(), lastName: c.Sobrenome.ToLower()));
+
+            return clientes.Generate(quantidade);
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(yearsToGoBack: 1, refDate: DateTime.Now.AddYears(1)),
+                    "",
+                    false,
+                    DateTime.Now));
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
