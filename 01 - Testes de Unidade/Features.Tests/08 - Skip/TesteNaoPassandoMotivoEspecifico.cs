e73c298de5e79c456c7a6ab441bce4c8fddab399
Bryan <bryanlima200@outlook.com>
2022-08-17 23:59:56 -0300

    Adicionado exemplo de como escapar de teste (pular), usando Skip Desta forma o teste não será executado, e será informado um warning

---
+++ 01 - Testes de Unidade/Features.Tests/08 - Skip/TesteNaoPassandoMotivoEspecifico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.Tests
+{
+    public class TesteNaoPassandoMotivoEspecifico
+    {
+        [Fact(DisplayName = "Novo Cliente 2.0", Skip = "Nova versão 2.0 quebrando")]
+        [Trait("Categoria", "Escapando dos Testes")]
+        public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
+        {
+            Assert.True(condition: false);
+        }
+    }
+}
