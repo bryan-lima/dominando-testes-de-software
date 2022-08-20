3f65b19b128b53dccb83e3ffac2d8c7fa71fc7a6
Bryan <bryanlima200@outlook.com>
2022-08-20 14:23:13 -0300

    Adicionado teste Voucher_ValidarVoucherPorcentagem_DeveEstarInvalido, funcionou de primeira Neste caso ao escrever o teste já passou sem falhas e sem necessidade de refatoração, pois é um teste que valida um cenário contrário (inválido) do teste anterior, no qual já foi feito código validando os cenários corretamente

--- 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -56,5 +56,25 @@ namespace NerdStore.Vendas.Domain.Tests
             // Assert
             Assert.True(result.IsValid);
         }
+
+        [Fact(DisplayName = "Validar Voucher Porcentagem Inválido")]
+        [Trait("Categoria", "Vendas - Voucher")]
+        public void Voucher_ValidarVoucherPorcentagem_DeveEstarInvalido()
+        {
+            var voucher = new Voucher("", null, null, TipoDescontoVoucher.Porcentagem, 0, DateTime.Now.AddDays(-1), false, true);
+
+            // Act
+            var result = voucher.ValidarSeAplicavel();
+
+            // Assert
+            Assert.False(result.IsValid);
+            Assert.Equal(6, result.Errors.Count);
+            Assert.Contains(VoucherAplicavelValidation.AtivoErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.CodigoErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.DataValidadeErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.QuantidadeErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.UtilizadoErroMsg, result.Errors.Select(c => c.ErrorMessage));
+            Assert.Contains(VoucherAplicavelValidation.PercentualDescontoErroMsg, result.Errors.Select(c => c.ErrorMessage));
+        }
     }
 }
