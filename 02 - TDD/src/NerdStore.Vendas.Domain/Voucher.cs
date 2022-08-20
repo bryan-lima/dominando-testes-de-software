124aa6588e1a3c7dada515a4a1e9909e8f816c42
Bryan <bryanlima200@outlook.com>
2022-08-20 13:44:03 -0300

    Adicionado teste Voucher_ValidarVoucherTipoValor_DeveEstarValido, etapa RED (ver o teste falhar)

---
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Voucher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Voucher
+    {
+        public string Codigo { get; set; }
+        public decimal? PercentualDesconto { get; set; }
+        public decimal? ValorDesconto { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime DataValidade { get; set; }
+        public bool Ativo { get; set; }
+        public bool Utilizado { get; set; }
+
+        public bool ValidarSeAplicavel()
+        {
+            return false;
+        }
+    }
+}
---
+++ 02 - TDD/tests/NerdStore.Vendas.Domain.Tests/VoucherTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NerdStore.Vendas.Domain.Tests
+{
+    public class VoucherTests
+    {
+        [Fact(DisplayName = "Validar Voucher Tipo Valor Válido")]
+        [Trait("Categoria", "Vendas - Voucher")]
+        public void Voucher_ValidarVoucherTipoValor_DeveEstarValido()
+        {
+            // Arrange
+            var voucher = new Voucher
+            {
+                Codigo = "PROMO-15-REAIS",
+                ValorDesconto = 15,
+                PercentualDesconto = null,
+                Quantidade = 1,
+                DataValidade = DateTime.Now,
+                Ativo = true,
+                Utilizado = false
+            };
+
+            // Act
+            var result = voucher.ValidarSeAplicavel();
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
