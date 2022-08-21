933c2d207a13c8b3622931b40755a114252e0a20
Bryan <bryanlima200@outlook.com>
2022-08-21 12:36:39 -0300

    Separado classes e enumeradores em arquivos distintos

--- 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -158,41 +158,4 @@ namespace NerdStore.Vendas.Domain
             }
         }
     }
-
-    public enum PedidoStatus
-    {
-        Rascunho = 0,
-        Iniciado = 1,
-        Pago = 4,
-        Entregue = 5,
-        Cancelado = 6
-    }
-
-    public class PedidoItem
-    {
-        public Guid ProdutoId { get; private set; }
-        public string ProdutoNome { get; private set; }
-        public int Quantidade { get; private set; }
-        public decimal ValorUnitario { get; private set; }
-
-        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
-        {
-            if (quantidade < Pedido.MIN_UNIDADES_ITEM) throw new DomainException($"Mínimo de {Pedido.MIN_UNIDADES_ITEM} unidades por produto");
-
-            ProdutoId = produtoId;
-            ProdutoNome = produtoNome;
-            Quantidade = quantidade;
-            ValorUnitario = valorUnitario;
-        }
-
-        internal void AdicionarUnidades(int quantidades)
-        {
-            Quantidade += quantidades;
-        }
-
-        internal decimal CalcularValor()
-        {
-            return Quantidade * ValorUnitario;
-        }
-    }
 }
--- 02 - TDD/src/NerdStore.Vendas.Domain/Voucher.cs
+++ 02 - TDD/src/NerdStore.Vendas.Domain/Voucher.cs
@@ -93,10 +93,4 @@ namespace NerdStore.Vendas.Domain
             return dataValidade >= DateTime.Now;
         }
     }
-
-    public enum TipoDescontoVoucher
-    {
-        Porcentagem = 0,
-        Valor = 1
-    }
 }
