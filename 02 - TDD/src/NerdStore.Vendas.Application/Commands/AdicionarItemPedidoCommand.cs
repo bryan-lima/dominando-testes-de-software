0ff568dbc014fda84043409da21966b9670ad6a9
Bryan <bryanlima200@outlook.com>
2022-08-21 13:03:20 -0300

    Adicionado teste AdicionarItemPedidoCommand_CommandEstaValido_DevePassarNaValidacao, etapa RED (ver o teste falhar)

---
+++ 02 - TDD/src/NerdStore.Vendas.Application/Commands/AdicionarItemPedidoCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class AdicionarItemPedidoCommand
+    {
+        public Guid ClienteId { get; set; }
+        public Guid ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+
+        public AdicionarItemPedidoCommand(Guid clienteId, Guid produtoId, string nome, int quantidade, decimal valorUnitario)
+        {
+            ClienteId = clienteId;
+            ProdutoId = produtoId;
+            Nome = nome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        public bool EhValido()
+        {
+            return false;
+        }
+    }
+}
