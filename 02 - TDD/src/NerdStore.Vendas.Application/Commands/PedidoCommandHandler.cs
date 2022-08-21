a960ffdd484a2c4c9b1e34e26eeba92bb27f3ed9
Bryan <bryanlima200@outlook.com>
2022-08-21 14:01:44 -0300

    Adicionado teste AdicionarItem_NovoPedido_DeveExecutarComSucesso, etapa RED (ver o teste falhar)

---
+++ 02 - TDD/src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class PedidoCommandHandler
+    {
+        public bool Handle(AdicionarItemPedidoCommand adicionarItemPedido)
+        {
+            return false;
+        }
+    }
+}
