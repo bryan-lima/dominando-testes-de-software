98ee17f8cd4f25cd5fd34b39918b8f375923860d
Bryan <bryanlima200@outlook.com>
2022-08-24 23:31:42 -0300

    Criado classe Startup, e movido configurações de Program para a Startup

--- 03 - Testes de Integracao/src/NerdStore.WebApp.MVC/Program.cs
+++ 03 - Testes de Integracao/src/NerdStore.WebApp.MVC/Program.cs
@@ -1,106 +1,13 @@
-using MediatR;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using NerdStore.Catalogo.Application.AutoMapper;
-using NerdStore.Catalogo.Data;
-using NerdStore.Vendas.Data;
-using NerdStore.WebApp.MVC.Data;
-using NerdStore.WebApp.MVC.Setup;
-using System.Reflection;
+using NerdStore.WebApp.MVC;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var startup = new Startup(builder.Configuration);
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDbContext<CatalogoContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDbContext<VendasContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Insira o token JWT desta maneira: Bearer {seu token}",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "SoftLima API",
-        Description = "SoftLima API",
-        Contact = new OpenApiContact { Name = "softlima.com", Email = "contato@softlima.com", Url = new Uri("http://softlima.com") },
-        License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
-    });
-});
-
-builder.Services.AddAutoMapper(typeof(DomainToViewModelMappingProfile), typeof(ViewModelToDomainMappingProfile));
-
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
-
-builder.Services.RegisterServices();
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Vitrine}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.UseSwagger();
-app.UseSwaggerUI(s =>
-{
-    s.SwaggerEndpoint("/swagger/v1/swagger.json", "SoftLima API v1.0");
-});
+startup.Configure(app, app.Environment);
 
 app.Run();
