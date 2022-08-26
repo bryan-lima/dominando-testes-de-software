45348bd23c2450f7f8ea3fce45d0a0acf9e41cf2
Bryan <bryanlima200@outlook.com>
2022-08-26 20:21:23 -0300

    Adicionado classe de testes UsuarioTests

---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -0,0 +1,20 @@
+﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class UsuarioTests
+    {
+        private readonly IntegrationTestsFixtures<StartupWebTests> _testsFixture;
+
+        public UsuarioTests(IntegrationTestsFixtures<StartupWebTests> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+    }
+}
