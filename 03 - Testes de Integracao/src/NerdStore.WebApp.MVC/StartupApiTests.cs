ef9c53fc2294082022460a572e68d2f054bf1b1f
Bryan <bryanlima200@outlook.com>
2022-08-26 00:21:30 -0300

    Adicionado setup básico de configurações para testes de integração

---
+++ 03 - Testes de Integracao/src/NerdStore.WebApp.MVC/StartupApiTests.cs
@@ -0,0 +1,118 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using NerdStore.Catalogo.Application.AutoMapper;
+using NerdStore.Catalogo.Data;
+using NerdStore.Vendas.Data;
+using NerdStore.WebApp.MVC.Data;
+using NerdStore.WebApp.MVC.Setup;
+using System.Reflection;
+
+namespace NerdStore.WebApp.MVC
+{
+    public class StartupApiTests : IStartup
+    {
+        public IConfiguration Configuration { get; }
+
+        public StartupApiTests(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDbContext<CatalogoContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDbContext<VendasContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "SoftLima API",
+                    Description = "SoftLima API",
+                    Contact = new OpenApiContact { Name = "softlima.com", Email = "contato@softlima.com", Url = new Uri("http://softlima.com") },
+                    License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+
+            services.AddAutoMapper(typeof(DomainToViewModelMappingProfile), typeof(ViewModelToDomainMappingProfile));
+
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            services.RegisterServices();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Vitrine}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "SoftLima API v1.0");
+            });
+        }
+    }
+}
---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixtures.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using NerdStore.WebApp.MVC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationWebTestsFixtureCollection))]
+    public class IntegrationWebTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixtures<StartupWebTests>> 
+    { 
+
+    }
+
+    [CollectionDefinition(nameof(IntegrationApiTestsFixtureCollection))]
+    public class IntegrationApiTestsFixtureCollection : ICollectionFixture<IntegrationTestsFixtures<StartupApiTests>>
+    {
+
+    }
+
+    public class IntegrationTestsFixtures<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly LojaAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixtures()
+        {
+            var clienteOptions = new WebApplicationFactoryClientOptions
+            {
+
+            };
+
+            Factory = new LojaAppFactory<TStartup>();
+            Client = Factory.CreateClient(clienteOptions);
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
