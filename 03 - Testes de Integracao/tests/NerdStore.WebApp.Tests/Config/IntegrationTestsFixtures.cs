d4e073a8d6ba3cba11e08f064d7afbbaf036dc1f
Bryan <bryanlima200@outlook.com>
2022-08-28 18:14:20 -0300

    Adicionado teste de integração AdicionarItem_NovoPedido_DeveRetornarComSucesso

--- 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixtures.cs
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixtures.cs
@@ -1,9 +1,11 @@
 ﻿using Bogus;
 using Microsoft.AspNetCore.Mvc.Testing;
 using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -28,6 +30,8 @@ namespace NerdStore.WebApp.Tests.Config
 
         public string UsuarioEmail;
         public string UsuarioSenha;
+        
+        public string UsuarioToken;
 
         public readonly LojaAppFactory<TStartup> Factory;
         public HttpClient Client;
@@ -51,6 +55,22 @@ namespace NerdStore.WebApp.Tests.Config
             var faker = new Faker("pt_BR");
             UsuarioEmail = faker.Internet.Email().ToLower();
             UsuarioSenha = faker.Internet.Password(length: 8, memorable: false, regexPattern: "", prefix: "@1Ab_");
+        }
+
+        public async Task RealizarLoginApi()
+        {
+            var userData = new LoginViewModel
+            {
+                Email = "teste@email.com",
+                Senha = "Teste*123"
+            };
+
+            // Recriando o client para evitar configurações de Web
+            Client = Factory.CreateClient();
+
+            var response = await Client.PostAsJsonAsync("api/login", userData);
+            response.EnsureSuccessStatusCode();
+            UsuarioToken = await response.Content.ReadAsStringAsync();
         }
 
         public async Task RealizarLoginWeb()
---
+++ 03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoApiTests.cs
@@ -0,0 +1,47 @@
+﻿using Features.Tests;
+using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.MVC.Models;
+using NerdStore.WebApp.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NerdStore.WebApp.Tests
+{
+    [TestCaseOrderer("Features.Tests.PriorityOrderer", "Features.Tests")]
+    [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+    public class PedidoApiTests
+    {
+        private readonly IntegrationTestsFixture<StartupApiTests> _testsFixture;
+
+        public PedidoApiTests(IntegrationTestsFixture<StartupApiTests> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Item em Novo Pedido"), TestPriority(1)]
+        [Trait("Categoria", "Integração API - Pedido")]
+        public async Task AdicionarItem_NovoPedido_DeveRetornarComSucesso()
+        {
+            // Arrange
+            var itemInfo = new ItemViewModel
+            {
+                Id = new Guid("A1BD239D-DF19-4305-B413-41F7C8CAC7E9"),
+                Quantidade = 2
+            };
+
+            await _testsFixture.RealizarLoginApi();
+            _testsFixture.Client.AtribuirToken(_testsFixture.UsuarioToken);
+
+            // Act
+            var postResponse = await _testsFixture.Client.PostAsJsonAsync("api/carrinho", itemInfo);
+
+            // Assert
+            postResponse.EnsureSuccessStatusCode();
+        }
+    }
+}
