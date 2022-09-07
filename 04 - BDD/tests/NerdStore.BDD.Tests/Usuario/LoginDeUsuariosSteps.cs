30eda4f388b74e29bd2c061c1009fdf12988b7e3
Bryan <bryanlima200@outlook.com>
2022-09-06 23:38:18 -0300

    Automatizado interface de cadastro e login

--- 04 - BDD/tests/NerdStore.BDD.Tests/Usuario/LoginDeUsuariosSteps.cs
+++ 04 - BDD/tests/NerdStore.BDD.Tests/Usuario/LoginDeUsuariosSteps.cs
@@ -1,27 +1,55 @@
+using NerdStore.BDD.Tests.Config;
 using System;
 using TechTalk.SpecFlow;
 
 namespace NerdStore.BDD.Tests.Usuario
 {
     [Binding]
+    [CollectionDefinition(nameof(AutomacaoWebFixtureCollection))]
     public class LoginDeUsuariosSteps
     {
+        private readonly LoginUsuarioTela _loginUsuarioTela;
+        private readonly AutomacaoWebTestsFixture _testsFixture;
+
+        public LoginDeUsuariosSteps(AutomacaoWebTestsFixture testsFixture)
+        {
+            _testsFixture = testsFixture;
+            _loginUsuarioTela = new LoginUsuarioTela(testsFixture.BrowserHelper);
+        }
+
         [When(@"Ele clicar em login")]
         public void WhenEleClicarEmLogin()
         {
-            throw new PendingStepException();
+            // Act
+            _loginUsuarioTela.ClicarNoLinkLogin();
+
+            // Assert
+            Assert.Contains(_testsFixture.Configuration.LoginUrl, _loginUsuarioTela.ObterUrl());
         }
 
         [When(@"Preencher os dados do formulário de login")]
         public void WhenPreencherOsDadosDoFormularioDeLogin(Table table)
         {
-            throw new PendingStepException();
+            // Arrange
+            var usuario = new Usuario
+            {
+                Email = "teste@email.com",
+                Senha = "Teste*123"
+            };
+            
+            _testsFixture.Usuario = usuario;
+
+            // Act
+            _loginUsuarioTela.PreencherFormularioLogin(usuario);
+
+            // Assert
+            Assert.True(_loginUsuarioTela.ValidarPreenchimentoFormularioLogin(usuario));
         }
 
         [When(@"Clicar no botão login")]
         public void WhenClicarNoBotaoLogin()
         {
-            throw new PendingStepException();
+            _loginUsuarioTela.ClicarNoBotaoLogin();
         }
     }
 }
